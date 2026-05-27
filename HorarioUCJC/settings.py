c5c9961ec7a8173d8053dea4ad4a8a6e5f85c2e2
Romendesu <rodrigojugon@gmail.com>
2026-05-27 19:59:02 +0200

    add generation commands

--- HorarioUCJC/settings.py
+++ HorarioUCJC/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0_(1io*qgk2&&h=p7d(0!b%p5#o(2sk@@r^_s294l4-^iw7ati'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0_(1io*qgk2&&h=p7d(0!b%p5#o(2sk@@r^_s294l4-^iw7ati')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
