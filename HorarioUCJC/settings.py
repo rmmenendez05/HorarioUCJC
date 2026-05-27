bd945fd5e77dceabc4ff07e77a92c9f5e2b36d5e
Romendesu <rodrigojugon@gmail.com>
2026-05-27 19:53:54 +0200

    add support for deploy

--- HorarioUCJC/settings.py
+++ HorarioUCJC/settings.py
@@ -143,9 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn HorarioUCJC.wsgi:application
